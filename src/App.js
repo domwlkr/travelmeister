b7cbaf6d980ea10e4ed15c8aba68104ea76740d4
Dominic Walker <dom@dominics-mbp.mynet>
2017-07-22 15:51:32 +0100

    Setup folder structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+const App = () => (
+    <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+            <h2>Travelmeister</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+        <p className="App-intro">A London Travel infromation service</p>
+    </div>
+);
 
 export default App;
